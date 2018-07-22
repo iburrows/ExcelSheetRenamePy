617711d0e24a53912d957a08f545796a1d396c55
Ian <ianburrows.sa@gmail.com>
2018-07-23 00:10:23 +0200

    add comments

--- ExcelSheetRenamePy/ExcelSheetRenamePy.py
+++ ExcelSheetRenamePy/ExcelSheetRenamePy.py
@@ -3,10 +3,11 @@ import os
 import tkinter
 from tkinter import filedialog
 
-
+#gets the workbook using a filechooser
 def getWorkBook():
     tkinter.Tk().withdraw()
     filepath = filedialog.askopenfilename()
+    #save the filename of the file for later
     filename = os.path.basename(filepath)
 
     wb = load_workbook(filename = filepath)
@@ -18,9 +19,12 @@ def setNewName(wb, filename):
     j = input("Starting number: ")
     i = int(j)
 
+    #loop through each sheet to  be able to change it
     for worksheets in wb:
         sheet = worksheets.title
+        #we don't want to rename testing overview and master data worksheets
         if sheet != "Testing Overview" and sheet != "Master Data":
+            #format will output 0 in front of single digit numbers e.g. 01,02 etc.
             worksheets.title = new_name + "{0:02}".format(i)
             print(worksheets.title)
             i += 1
